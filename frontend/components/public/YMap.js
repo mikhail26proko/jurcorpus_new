5e57c1c4a2e9775166d57ef02930c170418671bd
Михаил Прокофьев <mikhail26proko@gmail.com>
2023-03-27 21:19:59 +0300

    feat: move from lib yandex api to constructor script;

--- frontend/components/public/YMap.js
+++ frontend/components/public/YMap.js
@@ -4,20 +4,18 @@ import Script from 'next/script'
 
 const YMap = () => {
     return <>
-        <div className={style.ymaps} >
-            <Script
-                id="yandex_map"
-                src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9258bb772418d4001f36561b74613e0d98668d451d86dfe0598ae19c0b628971&amp;width=100%25&amp;height=534&amp;lang=ru_RU&amp;scroll=true"
-                strategy="lazyOnload"
-                async={false}
-                type="text/javascript"
-                onError={(e) => {
-                    console.error('Script failed to load', e)
-                }}
-            >
-            </Script>
-            <div id="map"></div>
-        </div>
+        <div id="yandex_map" className={style.ymap}></div>
+        <Script
+            id="yandex_map"
+            src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9258bb772418d4001f36561b74613e0d98668d451d86dfe0598ae19c0b628971&amp;width=100%25&amp;id=yandex_map&amp;lang=ru_RU&amp;scroll=true"
+            strategy="lazyOnload"
+            async={false}
+            type="text/javascript"
+            onError={(e) => {
+                console.error('Script failed to load', e)
+            }}
+        >
+        </Script>
     </>
 }
 
