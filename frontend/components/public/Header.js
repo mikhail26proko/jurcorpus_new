6114be5ab4faa3a818a25928badba3ba48bef5d9
Mikhail Prokofiev <mikhail26proko@gmail.com>
2023-07-19 09:45:22 +0300

    fix: Добавил теги og: в целях поисковой оптимизации;

--- frontend/components/public/Header.js
+++ frontend/components/public/Header.js
@@ -5,8 +5,12 @@ const Header = ( { language } ) => {
         <Head>
             <title>{language.title}</title>
             <link rel="icon" href="/favicon.ico" />
+            <meta name="title" content={language.title}></meta>
+            <meta name="og:title" content={language.title}></meta>
             <meta name="description" content={language.description}></meta>
+            <meta name="og:description" content={language.description}></meta>
             <meta name="keywords" content={language.keywords}></meta>
+            <meta name="og:keywords" content={language.keywords}></meta>
         </Head>
     </>
 }
