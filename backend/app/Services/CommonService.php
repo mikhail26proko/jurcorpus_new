38b06bd6203c267c8c670c342c96c25f106c2f59
Mikhail Prokofiev <mikhail26proko@gmail.com>
2024-03-15 14:52:59 +0300

    feat: Для сервисов добавил хуки pre и after;

--- backend/app/Services/CommonService.php
+++ backend/app/Services/CommonService.php
@@ -42,26 +42,67 @@ class CommonService
         return $entity;
     }
 
+    public function preCreate(array $data): array
+    {
+        return $data;
+    }
     public function create(array $data): Model
     {
+        if (empty($data = $this->preCreate($data))) {
+            throw new \Exception('Не удалось выполнить действие перед созданием');
+        }
+
         $entity = ($this->model)::create($data);
+
+        $this->afterCreate($data);
+
         return $this->get($entity->id);
     }
 
+    public function afterCreate(array $data): void
+    {}
+
+    public function preUpdate(int | string $id, array $data): array
+    {
+        return $data;
+    }
     public function update(int | string $id, array $data): Model
     {
+        if (empty($data = $this->preUpdate($id, $data)))
+        {
+            throw new \Exception('Не удалось выполнить действие перед обновлением');
+        }
+
         $entity = $this->get($id);
         $entity->fill($data)->save();
 
+        $this->afterUpdate($id, $data);
+
         return $this->get($entity->id);
+    }
+
+    public function afterUpdate(int | string $id, array $data): void
+    {}
+
+    public function preDelete(int | string $id): bool
+    {
+        return true;
     }
 
     public function delete(int | string $id): Model
     {
+        if (!$this->preDelete($id))
+        {
+            throw new \Exception('Не удалось выполнить действие перед удалением');
+        }
         $entity = $this->get($id);
         $entity -> delete();
+
+        $this->afterDelete($id);
 
         return $entity;
     }
 
+    public function afterDelete(int | string $id): void
+    {}
 }
