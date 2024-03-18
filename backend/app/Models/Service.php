d9b1e70b36b2352a2327912736fb623cb70999d1
Mikhail Prokofiev <mikhail26proko@gmail.com>
2024-03-19 00:57:58 +0300

    feat: Cортировки для услуг;

--- backend/app/Models/Service.php
+++ backend/app/Models/Service.php
@@ -3,14 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Orchid\Platform\Concerns\Sortable;
 
 class Service extends ExtendModel
 {
-    use SoftDeletes;
+    use SoftDeletes, Sortable;
 
     protected $fillable = [
         'title',
         'content',
         'sort',
     ];
+
+    public function getSortColumnName(): string
+    {
+        return 'sort';
+    }
+
 }
