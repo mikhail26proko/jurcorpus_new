d82c09e83ded2ae18caeedd212d56c1deb0e32cc
Mikhail Prokofiev <mikhail26proko@gmail.com>
2024-03-18 16:33:19 +0300

    feat: Добавил photo для Employee и поправил размер столбца для вакансий;

--- backend/app/Http/Resources/Employee/EmployeeResource.php
+++ backend/app/Http/Resources/Employee/EmployeeResource.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Employee;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\Branch\BranchResource;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
 
 class EmployeeResource extends JsonResource
 {
@@ -15,12 +15,15 @@ class EmployeeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $photo = $this->attachment()->first();
+
         return [
             'id'            => $this->id,
             'full_name'     => $this->full_name,
             'phone'         => $this->phone,
             'email'         => $this->email,
             'description'   => $this->description,
+            'photo'         => $photo ? $photo->url() : null ,
             'job_titles'    => $this->whenLoaded('job_titles',function(){ return array_column($this->job_titles->toArray(),'title');}),
             'directions'    => $this->whenLoaded('directions',function(){ return array_column($this->directions->toArray(),'title');}),
             'branch'        => $this->whenLoaded('branch', new BranchResource($this->branch)),
--- backend/database/migrations/2024_03_15_132607_create_vacansies_table.php
+++ backend/database/migrations/2024_03_15_132607_create_vacansies_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('vacansies', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->string('description', 5000);
             $table->timestamps();
             $table->softDeletes();
         });
